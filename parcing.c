16685cccf15add773a5ba39c25dd631ff4851c48
Youssef Bouaouda <ybouaoud@c4r8p2.1337.ma>
2024-03-31 23:50:17 +0000

    parcing almost done

--- parcing.c
+++ parcing.c
@@ -6,19 +6,20 @@
 /*   By: ybouaoud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 05:50:18 by ybouaoud          #+#    #+#             */
-/*   Updated: 2024/03/31 07:00:52 by ybouaoud         ###   ########.fr       */
+/*   Updated: 2024/03/31 23:49:37 by ybouaoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int dup_check(char **av, int ac)
+int	dup_check(char **av, int ac)
 {
-	int i;
-	int j;
-	int array[ac];
+	int	i;
+	int	j;
+	int	*array;
 
 	i = 0;
+	array = (int *)malloc(ac * sizeof(int));
 	while (av[i])
 	{
 		array[i] = ft_atoi(av[i]);
@@ -41,12 +42,32 @@ int dup_check(char **av, int ac)
 
 int	checker2(char *av)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (av[i])
 	{
-		if ((av[i] == '-' && !(av[i + 1])) || (av[i] == '+' && !(av[i + 1])))
+		if ((av[i] == '-' && (av[i + 1]) == ' ')
+			|| (av[i] == '+' && (av[i + 1]) == ' '))
+			return (1);
+		else if ((av[i] == '-' && !(av[i + 1]))
+			|| (av[i] == '+' && !(av[i + 1])))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	checker3(char *av)
+{
+	int	i;
+
+	i = 0;
+	while (av[i])
+	{
+		if ((av[i] <= 31) || (av[i] >= 33 && av[i] <= 44)
+			|| (av[i] >= 46 && av[i] <= 47)
+			|| (av[i] >= 58 && av[i] <= 127))
 			return (1);
 		i++;
 	}
@@ -55,9 +76,9 @@ int	checker2(char *av)
 
 int	checker(char **av, int ac)
 {
-	int i;
-	int j;
-	
+	int	i;
+	int	j;
+
 	i = 0;
 	if (dup_check(av, ac) == 1)
 		return (1);
@@ -67,14 +88,15 @@ int	checker(char **av, int ac)
 		while (av[i][j])
 		{
 			if (ft_isalpha(av[i][j]))
-				return(1);
-			else if ((av[i][j] == '-' || av[i][j] == '+') && (av[i][j + 1] == '-' || av[i][j + 1] == '+'))
+				return (1);
+			else if ((av[i][j] == '-' || av[i][j] == '+')
+					&& (av[i][j + 1] == '-' || av[i][j + 1] == '+'))
 				return (1);
 			j++;
 		}
-		if (checker2(av[i]) == 1)
+		if (checker2(av[i]) == 1 || checker3(av[i]) == 1)
 			return (1);
-		i++;	
+		i++;
 	}
 	return (0);
 }
